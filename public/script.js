744c00d5e300bd61ed592b2036ddcc8ae38e9375
Anders Kjær Damgaard <AndersKDamgaard@gmail.com>
2015-10-13 21:40:52 +0200

    Added client side movement.

--- public/script.js
+++ public/script.js
@@ -12,9 +12,31 @@ var context = canvas.getContext("2d");
 //Public variables
 var TAU = 2*Math.PI;
 var players = [];
+var playerID;
 var playerRadius = 20;
 var playerColor = "#333333";
 var backgroundColor = "#CCCCCC";
+
+document.addEventListener("keydown", function(e) {
+	var deltaX, deltaY;
+	
+	if (e.keyCode === 37)
+		deltaX--;
+	if (e.keyCode === 38)
+		deltaY--;
+	if (e.keyCode === 39)
+		deltaX++;
+	if (e.keyCode === 40)
+		deltaY++;
+
+	players.forEach(function(element, index) {
+        if (element.id === playerID) {
+            players[index].x += deltaX;
+            players[index].y += deltaY;
+            socket.emit('positionUpdate', {x: players[index].x, y: players[index].y});
+        }
+    });
+}, false);
 
 drawPlayer = function(player){
 	context.beginPath();
@@ -35,6 +57,10 @@ draw = function(){
 }
 
 var socket = io.connect();
+
+socket.on('myPlayerID', function(playerID) {
+	window.playerID = playerID;
+});
 
 socket.on('update', function(players) {
 	window.players = players;
