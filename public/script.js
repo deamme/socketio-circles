ba4697f6b7b89ea11fc2dc3a8e59a7a41002f50e
Anders Kjær Damgaard <AndersKDamgaard@gmail.com>
2015-10-13 16:59:44 +0200

    Public changes.

--- public/script.js
+++ public/script.js
@@ -1,4 +1,41 @@
-document.getElementById("sendButton").addEventListener(function (e) {
-	e.preventDefault();
-	//Socket code here
-});
+var canvas = document.getElementById("canvas");
+
+//Set size
+var width = 500;
+var height = 500;
+canvas.setAttribute("width",width);
+canvas.setAttribute("height",height);
+
+//Set context
+var context = canvas.getContext("2d");
+
+//Public variables
+var TAU = 2*Math.PI;
+var players = [];
+var playerRadius = 20;
+
+randomMinMax = function(min, max){
+	return Math.random() * (max - min) + min;
+}
+
+spawnPlayer = function(){
+	var player;
+
+	player.x = randomMinMax(0,width);
+	player.y = randomMinMax(0,height);
+
+	return player;
+}
+
+drawPlayer = function(player){
+	context.arc(player.x,player.y,playerRadius,0,TAU);
+	context.fillStyle = "#444444";
+	context.fill();
+}
+
+draw = function(){
+	fillRect();
+	for (player in players) {
+		drawPlayer(player);
+	}
+}
