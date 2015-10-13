11d2dc2972c9a8b6a97e62f4511a914183c3b9b7
Deam <deamme@users.noreply.github.com>
2015-10-13 17:08:03 +0200

    server and clien-side changes

--- public/script.js
+++ public/script.js
@@ -14,19 +14,6 @@ var TAU = 2*Math.PI;
 var players = [];
 var playerRadius = 20;
 
-randomMinMax = function(min, max){
-	return Math.random() * (max - min) + min;
-}
-
-spawnPlayer = function(){
-	var player;
-
-	player.x = randomMinMax(0,width);
-	player.y = randomMinMax(0,height);
-
-	return player;
-}
-
 drawPlayer = function(player){
 	context.arc(player.x,player.y,playerRadius,0,TAU);
 	context.fillStyle = "#444444";
--- server.js
+++ server.js
@@ -11,4 +11,24 @@ app.get('/test', function(req,res){
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+randomMinMax = function(min, max){
+    return Math.random() * (max - min) + min;
+}
+
+spawnPlayer = function(){
+    var player;
+
+    player.x = randomMinMax(0,width);
+    player.y = randomMinMax(0,height);
+
+    return player;
+}
+
+io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+        socket.on('my other event', function (data) {
+            console.log(data);
+    });
+});
+
 server.listen(3000);
